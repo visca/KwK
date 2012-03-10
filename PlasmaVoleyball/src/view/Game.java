ade4cac8dd0efde1b0f2b6ae3bfe341af1641c5c
Wojciech Woszczak <woszczak@gmail.com>
2012-03-10 15:40:05 +0100

    WW

---
+++ PlasmaVoleyball/src/view/Game.java
@@ -0,0 +1,19 @@
+package view;
+
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import view.state.*;
+
+
+public class Game {
+
+    public static void main(String[] args) {
+        GameFrame frame = new GameFrame(800, 600);
+        frame.setVisible(true);
+    }
+
+    
+}
